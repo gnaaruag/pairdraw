f20a0399281564f6fb4e71e83daffdda280ea2ef
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-04-14 21:44:18 +0530

    fix signup /sso-redirect error

--- pairdraw-front/src/main.tsx
+++ pairdraw-front/src/main.tsx
@@ -15,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider 
     publishableKey={PUBLISHABLE_KEY} 
+      afterSignUpUrl="/home"
 >
       <App />
     </ClerkProvider>
