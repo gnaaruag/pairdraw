a2d3c9b007ca1bf9b52209fdb440ab86f564ef85
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-04-15 21:40:08 +0530

    cors configuration

--- pairdraw-back/server.js
+++ pairdraw-back/server.js
@@ -1,28 +1,32 @@
-require ('dotenv').config();
-const bodyparser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./database/db')
+require("dotenv").config();
+const bodyparser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./database/db");
 
-const app = express() 
+const app = express();
 connectDB();
 
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use((express.static('public')))
-app.set('view engine','ejs');
-app.set('views','views'); 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const corsOptions = {
+  origin: ["https://pairdraw.vercel.app", "http://localhost:5173"],
+};
 
 // declare routes
 //const someRoute = require('./routes/...')
 
-app.use(express.json()) 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyparser.urlencoded({ extended: false }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 // enable routes
 // app.use('/',someRoute);
@@ -31,16 +35,14 @@ const pairings = require("./routes/pairing.route");
 
 app.use("/", pairings);
 
-
-app.get('*', (request, response) => {
-	response.send(404, "Not Found");
-})
+app.get("*", (request, response) => {
+  response.send(404, "Not Found");
+});
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () =>
-{   
-    console.log(`listening on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
 
 module.exports = app;
