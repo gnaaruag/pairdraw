7c2e03a00074f4590071ea087c0046c34c8c606f
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-04-16 11:52:03 +0530

    cleaner onboard ux

--- pairdraw-front/src/pages/onboard.tsx
+++ pairdraw-front/src/pages/onboard.tsx
@@ -21,12 +21,18 @@ const Onboard: FunctionComponent = () => {
       <div className="flex flex-col items-center justify-center max-w-md w-full px-6 py-8  rounded-lg ">
         <h1 className="text-2xl">Pairdraw</h1>
         <SignedOut>
+          <div className="mt-2 mb-2 flex flex-col gap-2 justify-center items-center mx-auto">
+          <h3 className="text-lg">If you do not have an account </h3>
+          <a className="text-purple-400 underline" href="https://main-walrus-3.accounts.dev/sign-up?redirect_url=https%3A%2F%2Fpairdraw.vercel.app%2Fhome">Click here to make one</a>
+          </div>
           <SignIn redirectUrl={"/home"} routing="path" path="/" />
         </SignedOut>
         <SignedIn>
           <div className="mt-2 flex flex-col gap-2 justify-center items-center mx-auto">
             <h3 className="text-xl">You are already logged in</h3>
-            <a href="/home" className="text-purple-400">Go to home page</a>
+            <a href="/home" className="text-purple-400">
+              Go to home page
+            </a>
           </div>
         </SignedIn>
       </div>
